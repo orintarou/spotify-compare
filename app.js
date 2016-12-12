2181f0ec8b0bee544e8ce4d9ab084f4c87d81bcc
Kumaran Chanthrakumar <kumaranc46@gmail.com>
2016-12-12 03:00:36 -0500

    input field for artist name

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-var CommentBox = React.createClass({
-  render: function() {
-    return(
-      <div className="commentBox">
-        yo
-      </div>
-    );
-  }
-});
-ReactDOM.render(
-  <CommentBox />,
-  document.getElementById('root')
+const counter = (state, action) => {
+      return state;
+}
+
+var setName = function(e) {
+    this.setState({value: e.target.value});
+}
+
+//DOM Component
+const FirstArtist = ({
+  value
+}) => (
+  <div>
+    <input type="text" defaultValue={value}/>
+  </div>
 );
+
+import { createStore } from 'redux';
+const store = createStore(counter);
+
+const render = () => {
+  ReactDOM.render(
+    <FirstArtist
+      value={store.getState()}
+    />,
+    document.getElementById('root')
+  );
+};
+
+store.subscribe(render);
+render();
