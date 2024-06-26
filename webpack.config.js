3b9a788763a26214a5f7c557b235b8358987d404
orintarou <kchan440@gmail.com>
2024-06-26 11:18:40 -0400

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: [ // Load webpac-dev-server and webpack modules. Load index.js as entry point.
-    'webpack-dev-server/client?http://127.0.0.1:8000',
+    'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
     './src/index.js'
   ],
@@ -24,8 +24,6 @@ module.exports = {
     filename: 'bundle.js' // Name of the JS bundle file
   },
   devServer: {
-    inline:true,
-    port:8000,
     contentBase: './dist', // Target directory of the build code
   }
 };
